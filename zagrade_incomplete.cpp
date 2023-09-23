ea7989e4be1714284ee471d475dac84b1685ef61
Rasib0 <rasibnadeem101@gmail.com>
2023-09-23 18:25:40 +0500

    Update 23/9/23

--- zagrade_incomplete.cpp
+++ zagrade_incomplete.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <iostream>
-#include <unordered_map>
+#include <stack>
 #include <vector>
 
 using namespace std;
@@ -10,43 +10,40 @@ int main() {
   cin >> s;
 
   vector<pair<int, int>> bracket_pairs;
-  vector<string> ans;
+  vector<string> list;
+
+  stack<int> open_brackets;
 
   for (int i = 0; i < s.size(); i++) {
     if (s[i] == '(') {
-      // save the index of the open bracket
-      // and -1 for the close bracket
-      bracket_pairs.push_back(make_pair(i, -1));
+      open_brackets.push(i);
+
     } else if (s[i] == ')') {
-      // find the last open bracket
-      // and set the close bracket index
-      for (int j = bracket_pairs.size() - 1; j >= 0; j--) {
-        if (bracket_pairs[j].second == -1) {
-          bracket_pairs[j].second = i;
-          break;
-        }
-      }
+      int open_index = open_brackets.top();
+      open_brackets.pop();
+      bracket_pairs.push_back(make_pair(open_index, i));
     }
   }
 
   // for each subset of brackets store the string in ans
-  for (int i = 1; i < (1 << bracket_pairs.size()); i++) {
+  for (int i = 1; i <= (1 << bracket_pairs.size()); i++) {
     string temp = s;
     for (int j = 0; j < bracket_pairs.size(); j++) {
       // if the jth bit is set, remove the jth bracket pair
-      if ((i & (1 << j)) >> j) {
-        temp[bracket_pairs[j].first] = ' ';
-        temp[bracket_pairs[j].second] = ' ';
+      if (i & (1 << j)) {
+        temp[bracket_pairs[j].first] = '$';
+        temp[bracket_pairs[j].second] = '$';
       }
     }
 
     // remove all spaces
-    temp.erase(remove(temp.begin(), temp.end(), ' '), temp.end());
-    ans.push_back(temp);
+    temp.erase(remove(temp.begin(), temp.end(), '$'), temp.end());
+    list.push_back(temp);
   }
 
-  sort(ans.begin(), ans.end());
-  for (auto it = ans.begin(); it != ans.end(); it++) {
-    cout << *it << endl;
+  sort(list.begin(), list.end());
+
+  for (auto &element : list) {
+    cout << element << endl;
   }
 }
