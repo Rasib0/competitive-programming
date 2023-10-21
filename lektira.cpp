05d4a447af3167187c038e88fd6181d24fca5bdb
Rasib0 <rasibnadeem101@gmail.com>
2023-10-21 15:43:30 +0500

    hi

--- lektira.cpp
+++ lektira.cpp
@@ -5,18 +5,22 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
   string input;
   cin >> input;
   vector<string> list;
 
-  for (int i = 0 + 1; i < input.size(); i++) {
-    for (int j = (i + 1) + 1; j < input.size(); j++) {
+  for (int i = 0 + 1; i < input.size(); i++)
+  {
+    for (int j = (i + 1) + 1; j < input.size(); j++)
+    {
 
       string first_word = input.substr(0, i);
       string second_word = input.substr(i, j - i);
       string third_word = input.substr(j);
 
+      reverse(first_word.begin(), first_word.end());
       reverse(second_word.begin(), second_word.end());
       reverse(third_word.begin(), third_word.end());
 
