a9c77b5489df03db18fae31f1d72da44ff67f2e4
Rasib0 <rasibnadeem101@gmail.com>
2023-10-27 19:33:06 +0500

    muzicari

--- muzicari.cpp
+++ muzicari.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 
 using namespace std;
-int main()
-{
+int main() {
   int T, N;
   cin >> T >> N;
 
@@ -12,24 +11,18 @@ int main()
   vector<bool> used(N, false);
 
   vector<vector<int>> dp(N + 1, vector<int>(T + 1, 0));
-  for (int i = 0; i < N; i++)
-  {
+  for (int i = 0; i < N; i++) {
     cin >> w[i];
     p[i] = w[i]; // don't really need a seperate profit vector
   }
 
-  for (int i = 0; i <= N; i++)
-  {
-    for (int j = 0; j <= T; j++)
-    {
+  for (int i = 0; i <= N; i++) {
+    for (int j = 0; j <= T; j++) {
       if (i == 0 or j == 0)
         dp[i][j] = 0;
-      else if (w[i - 1] <= j)
-      {
+      else if (w[i - 1] <= j) {
         dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i - 1]] + p[i - 1]);
-      }
-      else
-      {
+      } else {
         dp[i][j] = dp[i - 1][j];
       }
     }
@@ -38,10 +31,8 @@ int main()
   int ii = N;
   int jj = T;
   // backtrack to find the solution set
-  while (ii > 0 and jj > 0)
-  {
-    if (dp[ii][jj] != dp[ii - 1][jj])
-    {
+  while (ii > 0 and jj > 0) {
+    if (dp[ii][jj] != dp[ii - 1][jj]) {
       jj -= w[ii - 1];
       used[ii - 1] = true;
     }
@@ -51,15 +42,11 @@ int main()
   // print the solution
   ii = 0;
   jj = 0;
-  for (int i = 0; i < N; i++)
-  {
-    if (used[i])
-    {
+  for (int i = 0; i < N; i++) {
+    if (used[i]) {
       cout << ii << " ";
       ii += w[i];
-    }
-    else
-    {
+    } else {
       cout << jj << " ";
       jj += w[i];
     }
