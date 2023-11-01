6725667d5a6b46a76ef3363324888ed308389831
Rasib0 <rasibnadeem101@gmail.com>
2023-11-01 21:33:06 +0500

    Completed Restaurant orders in both py and cpp

--- restaurantorders.cpp
+++ restaurantorders.cpp
@@ -1,94 +1,80 @@
-#include <algorithm>
 #include <iostream>
-#include <limits>
-#include <numeric>
-#include <set>
-#include <unordered_map>
 #include <utility>
 #include <vector>
 
-#define int long long int
 #define endl "\n"
-#define vec vector
 #define IOS                                                                    \
   ios::sync_with_stdio(0);                                                     \
   cin.tie(0);                                                                  \
   cout.tie(0);
-
 using namespace std;
 
-struct PairHash {
-  template <typename T1, typename T2>
-  auto operator()(const pair<T1, T2> &p) const -> size_t {
-    return (hash<T1>{}(p.first) << 32) | hash<T2>{}(p.second);
-  }
-};
-// Coin Change 2 - LeetCode
+// pair <prev, element>
+void combination_sum(int T, vector<int> &v, vector<vector<pair<int, int>>> &dp,
+                     vector<int> &count) {
+  for (int i = 0; i < v.size(); i++) {
+    int c = v[i];
+    for (int j = c; j <= T; j++) {
 
-void combination_sum(int T, int index, vector<int> &v, int &count,
-                     vector<int> &path, vec<int> &ans,
-                     unordered_map<pair<int, int>, int, PairHash> &dp) {
-  if (T < 0) {
-    return;
-  }
+      if (j == c) {
+        dp[j].push_back({-1, i});
+        count[j]++;
+      }
 
-  if (dp.find({T, index}) != dp.end()) {
-    count += dp[{T, index}];
-    return;
-  }
-
-  // if (count > 1) {
-  //   return;
-  // }
-
-  if (T == 0) {
-    count++;
-    ans = path;
-    return;
-  }
-
-  for (int i = index; i < v.size(); i++) {
-    path.push_back(i + 1);
-    combination_sum(T - v[i], i, v, count, path, ans, dp);
-    dp[{T, i}] = count;
-    path.pop_back();
+      if (count[j - c] > 1) {
+        count[j] = 2;
+      } else {
+        // for (auto comb : dp[j - c]) {
+        for (int k = 0; k < dp[j - c].size(); k++) {
+          dp[j].push_back({j - c, i});
+          count[j]++;
+        }
+      }
+    }
   }
 }
 
 int32_t main() {
-  IOS;
-
-  // input
   int N;
   cin >> N;
-  vec<int> v(N);
+  vector<int> prices(N);
   for (int i = 0; i < N; i++) {
-    cin >> v[i];
+    cin >> prices[i];
   }
 
   int M;
   cin >> M;
-  vec<int> t(M);
+  vector<int> t(M);
+  int max_target = 0;
   for (int i = 0; i < M; i++) {
     cin >> t[i];
+    if (max_target < t[i]) {
+      max_target = t[i];
+    }
   }
 
-  // solve for each testcases
-  for (int i = 0; i < M; i++) {
-    vec<int> path_dummy;
-    vec<int> ans;
+  vector<int> count(max_target + 1);
+  vector<vector<pair<int, int>>> dp(max_target + 1, vector<pair<int, int>>());
 
-    unordered_map<pair<int, int>, int, PairHash> dp;
-    int count = 0;
-    combination_sum(t[i], 0, v, count, path_dummy, ans, dp);
+  combination_sum(max_target, prices, dp, count);
 
-    if (count == 0)
+  for (auto target : t) {
+    if (count[target] == 0) {
       cout << "Impossible" << endl;
-    else if (count > 1)
+    } else if (count[target] > 1) {
       cout << "Ambiguous" << endl;
-    else {
-      for (auto x : ans) {
-        cout << x << " ";
+    } else {
+      auto pair = dp[target][0];
+      vector<int> ans;
+      ans.push_back(pair.second);
+
+      while (pair.first != -1 and pair.second != 0) {
+        pair = dp[pair.first][0];
+        ans.push_back(pair.second);
+      }
+
+      for (int i = ans.size() - 1; i >= 0; i--) {
+        cout << ans[i] + 1 << " ";
       }
       cout << endl;
     }
